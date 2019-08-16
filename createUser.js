830f36df98d446a7ee8c81798a69fffc25f7af89
ioviic <>
2019-08-16 23:54:31 +0300

    extract some code into libs common place

--- createUser.js
+++ createUser.js
@@ -1,9 +1,8 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
+import * as dynamoDbLib from './libs/dynamodb-lib'
+import { failure, success } from './libs/response-lib'
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
+export async function main(event, context, callback) {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
   const userId = uuid.v1();
@@ -35,30 +34,38 @@ export function main(event, context, callback) {
     }
   };
 
-  dynamoDb.put(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
 
-    // Return status code 500 on error
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false })
-      };
-      callback(null, response);
-      return;
-    }
 
-    // Return status code 200 and the newly created item
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
-  });
+  // dynamoDb.put(params, (error, data) => {
+  //   // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+  //   const headers = {
+  //     "Access-Control-Allow-Origin": "*",
+  //     "Access-Control-Allow-Credentials": true
+  //   };
+  //
+  //   // Return status code 500 on error
+  //   if (error) {
+  //     const response = {
+  //       statusCode: 500,
+  //       headers: headers,
+  //       body: JSON.stringify({ status: false })
+  //     };
+  //     callback(null, response);
+  //     return;
+  //   }
+  //
+  //   // Return status code 200 and the newly created item
+  //   const response = {
+  //     statusCode: 200,
+  //     headers: headers,
+  //     body: JSON.stringify(params.Item)
+  //   };
+  //   callback(null, response);
+  // });
 }
